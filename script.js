76eff7698dc1ae609f630e8231ed625272479724
silvestrin.m <mateus_silvestrin@yahoo.com.br>
2021-10-18 20:01:59 -0300

    Mostrar numero unico.

---
+++ script.js
@@ -0,0 +1,10 @@
+let visor = document.getElementById("visor");
+
+const addText = function(calc_input){
+    return visor.innerText = calc_input;
+}
+
+//visor.innerText = "Wow!"
+// visor.append("What?!")
+
+console.log(addText);
