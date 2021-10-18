8d2bff76344733f2f10608f92b60d91ebd04fb63
silvestrin.m <mateus_silvestrin@yahoo.com.br>
2021-10-18 20:27:24 -0300

    Numeros e clear funcionando.

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let visor = document.getElementById("visor");
 
 const addText = function(calc_input){
-    return visor.innerText = calc_input;
+    visor.innerText = visor.innerText.trimEnd().concat(calc_input);
+}
+
+const clearAll = function(){
+    //window.alert('funciona!')
+    visor.innerText = null;
 }
 
 //visor.innerText = "Wow!"
 // visor.append("What?!")
-
-console.log(addText);
