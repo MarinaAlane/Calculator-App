c53b20f9061d8aea556daffc95b3c61f6b499f19
silvestrin.m <mateus_silvestrin@yahoo.com.br>
2021-10-23 12:17:36 -0300

    Beautiful, but bleeding.

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let visor = document.getElementById("visor");
+let visorText = document.getElementById("visor");
 
 const addText = function(calc_input){
-    visor.innerText = visor.innerText.trimEnd().concat(calc_input);
+    visorText.innerHTML = visorText.innerHTML.trimEnd().concat(calc_input);
 }
 
 const clearAll = function(){
     //window.alert('funciona!')
-    visor.innerText = null;
+    visorText.innerHTML = '&nbsp;';
 }
 
 //visor.innerText = "Wow!"
