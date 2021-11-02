a80f1fe826b8b5b42285f2a83e99dfbb0ad08669
silvestrin.m <mateus_silvestrin@yahoo.com.br>
2021-11-01 22:21:46 -0300

    Limpeza esboço. Add comentários.

--- script.js
+++ script.js
@@ -13,52 +13,20 @@ const showResult = function(result){
     visorText.innerText = result;
 }
 
-/* 
-Execução das operações.
-Etapas:
-1- Encontrar e fazer multiplicações
-*/
-
-let teste = '1+2*2+2*3/2-1';
-let multiplicacoes = teste.match(/\d+\*\d+/g);
- 
-multiplicacoes = 
-    multiplicacoes.map(i => i.split('*')). // array de arrays com conjuntos de numeros
-    map(n => n.flatMap(nn => parseFloat(nn))). // transformar strings em numeros
-    map(m => m.reduce((mm1, mm2) => mm1*mm2)); // fazer as multiplicações de cada bloco
-console.log(multiplicacoes);
-
-//2- Remontar expressão substituindo resultados das multiplicações
-teste = multiplicacoes.reduce((c,m) => c.replace(/\d+\*\d+/,m),teste);
-console.log(teste)
-
-//3 -Repetir processo para divisões
-let divisoes = teste.match(/\d+\/\d+/g);
-divisoes =
-    divisoes.map(i => i.split('/')). 
-        map(n => n.flatMap(nn => parseFloat(nn))). 
-        map(m => m.reduce((mm1, mm2) => mm1/mm2));
-teste = divisoes.reduce((c,m) => c.replace(/\d+\/\d+/,m),teste);
-console.log(teste)
-
-//4- Somas e subtrações
-let somasSubtr = teste.match(/\+\d+|^\d+|\-\d+/g);
-console.log(
-    somasSubtr.map(a => parseFloat(a.replace("+",""))).
-    reduce((b,c) => b+c)
-    );
-
+//Execução das operações.
 const equal = function(){
     let expressao = visorText.innerText;
+    //Se houver multiplicação, fazer
     if (expressao.match(/\*/)){
         multiplicacoes = expressao.match(/\d+\*\d+/g);
         multiplicacoes = 
             multiplicacoes.map(i => i.split('*')). // array de arrays com conjuntos de numeros
                 map(n => n.flatMap(nn => parseFloat(nn))). // transformar strings em numeros
                 map(m => m.reduce((mm1, mm2) => mm1*mm2)); // fazer as multiplicações de cada bloco
+        // Remontar expressão substituindo resultados das multiplicações
         expressao = multiplicacoes.reduce((c,m) => c.replace(/\d+\*\d+/,m),expressao);
     }
-
+    //Se houver divisão, fazer
     if (expressao.match('\/')){
         let divisoes = expressao.match(/\d+\/\d+/g);
         divisoes =
@@ -67,11 +35,14 @@ const equal = function(){
                 map(m => m.reduce((mm1, mm2) => mm1/mm2));
         expressao = divisoes.reduce((c,m) => c.replace(/\d+\/\d+/,m), expressao);
     }
-
+    // Somas e subtrações em uma tacada só.
+    // Roda se houver pelo menos uma expressão completa de soma ou subtração
     if(expressao.match(/^\d+\+\d+|^\d+-\d+/)){
     let somasSubtr = expressao.match(/\+\d+|^\d+|\-\d+/g);
-     showResult(somasSubtr.map(a => parseFloat(a.replace("+",""))).
-    reduce((b,c) => b+c));
+    // mostra resultado no visor com showResult
+    showResult(somasSubtr.
+        map(a => parseFloat(a.replace("+",""))).
+        reduce((b,c) => b+c));
     } else {
         return expressao
     }
