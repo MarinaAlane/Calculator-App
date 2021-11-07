31af52af12c1c33c5342f61ac79bbeb4643a0e09
silvestrin.m <mateus_silvestrin@yahoo.com.br>
2021-11-07 18:42:06 -0300

    Funções separadas para operações.

--- script.js
+++ script.js
@@ -13,6 +13,32 @@ const showResult = function(result){
     visorText.innerText = result;
 }
 
+const multiplicacao = function(expressao){
+    multiplicacoes = expressao.match(/\d+\*\d+/g);
+        multiplicacoes = 
+            multiplicacoes.map(i => i.split('*')). // array de arrays com conjuntos de numeros
+                map(n => n.flatMap(nn => parseFloat(nn))). // transformar strings em numeros
+                map(m => m.reduce((mm1, mm2) => mm1*mm2)); // fazer as multiplicações de cada bloco
+        // Remontar expressão substituindo resultados das multiplicações
+    return multiplicacoes.reduce((c,m) => c.replace(/\d+\*\d+/,m),expressao)
+}
+
+const divisao = function(expressao){
+    let divisoes = expressao.match(/\d+\/\d+/g);
+        divisoes =
+            divisoes.map(i => i.split('/')). 
+                map(n => n.flatMap(nn => parseFloat(nn))). 
+                map(m => m.reduce((mm1, mm2) => mm1/mm2));
+    return divisoes.reduce((c,m) => c.replace(/\d+\/\d+/,m), expressao)
+}
+
+const somaSubtracao = function(expressao){
+    let somasSubtr = expressao.match(/\+\d+|^\d+|\-\d+/g);
+    return somasSubtr.
+    map(a => parseFloat(a.replace("+",""))).
+    reduce((b,c) => b+c)
+}
+
 //Execução das operações.
 const equal = function(){
     let expressao = visorText.innerText;
